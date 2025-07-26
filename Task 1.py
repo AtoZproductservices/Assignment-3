20f64e86f372372afca03c33cd7d74446f3befac
AtoZproductservices <77913594+AtoZproductservices@users.noreply.github.com>
2025-07-26 08:11:22 +0530

    Create Task 1.py

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+#Task 1: Calculate Factorial Using a Function
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+num=int(input('Enter a number: '))
+result=factorial(num)
+print('Factorial of ',num,' is: ',result)
