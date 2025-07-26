864b3ac85f5980e58e2ea5d5d43e755694712c6f
AtoZproductservices <77913594+AtoZproductservices@users.noreply.github.com>
2025-07-26 08:11:41 +0530

    Create Task 2.py

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+#Task 2: Using the Math Module for Calculations
+num=int(input('Enter a number: '))
+
+from math import *
+print('Square root: ',sqrt(num))
+print('Logarithm: ',log(num))
+print('Sine: ',sin(num))
